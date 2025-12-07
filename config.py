88f7b3982944e215d3ae101107995f81c0248365
hiddenluck <ekekhong@gmail.com>
2025-12-07 21:12:30 +0900

    Remove API keys from configuration file보안
    보안문제

--- config.py
+++ config.py
@@ -1,5 +1,1 @@
 # VS Code 로컬 환경 설정 파일 (Git 커밋 금지)
-
-# !!! 여기에 대표님의 실제 Gemini API 키를 넣어주세요. !!!
-GEMINI_API_KEY = "AIzaSyBILo7XwKAE0X96rseLP5hkcugx_4dBPNM"
-KASI_API_KEY = "ba8ceb1f5394b9e108fd72159ee26daf62de3ae9566d3d13b5afcb0bb52b9689"
